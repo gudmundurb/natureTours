f5d4ada012a3df6d4699d795ab62b60271eab985
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-03 21:31:55 +0000

    refactoring for MVC architecture

--- server.js
+++ server.js
@@ -13,38 +13,11 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497
-});
-
-testTour
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => console.log('Database error: ', err));
 //Server setup
 const PORT = process.env.PORT || 8000;
 
