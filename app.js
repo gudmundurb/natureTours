502e2ecbbefb90a9c24487e5f99046db720cff1c
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-03 20:23:09 +0000

    database connected (mongoDB Atlas servers)

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 //----------------------//
 //Middle Ware           //
 //----------------------//
--- server.js
+++ server.js
@@ -1,10 +1,51 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-//console.log(process.env);
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log('Database error: ', err));
+//Server setup
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
