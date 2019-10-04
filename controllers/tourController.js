91f15b878d1a2589d2674514d677c839a7ab5c53
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-04 10:07:31 +0000

    filtering and sorting implemented, logging fixed in server, tourModel expanded, importing script created

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,34 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build Query!
+    // 1a) filtering
+    const queryObj = { ...req.query };
+    console.log(queryObj);
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    // 1b) advanced filtering
+    // { difficulty: 'easy', duration: {$gte: 5 } } what we want (notice $)
+    // { difficulty: 'easy', duration: { gte: '5' }, } how it comes out
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // gte, gt, lte, lt
+    let query = Tour.find(JSON.parse(queryStr));
+    console.log(queryStr);
+    //2 sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt'); //shows new tours first
+    }
+
+    //Execute Query!
+    const tours = await query;
+
+    //Send response!
     res.status(200).json({
       status: 'Success',
       results: tours.length,
@@ -50,7 +76,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!'
+      message: err
     });
   }
 };
@@ -77,18 +103,15 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
+    await Tour.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
       status: 'Success',
-      data: {
-        tour,
-        message: 'Document deleted successfully'
-      }
+      data: null
     });
   } catch (err) {
     res.status(404).json({
-      status: 'Success',
+      status: 'Fail',
       message: err
     });
   }
