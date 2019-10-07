eaa7f1df1a666cabbb6900f8270f1f9efb77349d
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-07 13:51:01 +0000

    more refactoring APIFeatures class moved to utils folder in a separated file

---
+++ utils/apiFeatures.js
@@ -0,0 +1,54 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // { difficulty: 'easy', duration: {$gte: 5 } } what we want (notice $)
+    // { difficulty: 'easy', duration: { gte: '5' }, } how it comes out
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // gte, gt, lte, lt
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt'); //shows new tours first
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination() {
+    const LIMIT = 10;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || LIMIT;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
