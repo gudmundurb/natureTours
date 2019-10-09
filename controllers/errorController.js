3975f46b16f2f3a985dc478a80bff60b6fd44f56
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-09 11:05:56 +0000

    token verification and expiration added, protected route middleware created

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -19,6 +19,11 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = err =>
+  new AppError('Ooops.. your token is wrong. Please log in again!', 401);
+
+const handleJWTExpiredError = err =>
+  new AppError('Ooops... your token has expired. Please log in again!', 401);
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -64,7 +69,9 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
-
+    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
     sendErrorProd(error, res);
   }
 };
