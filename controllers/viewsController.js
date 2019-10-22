30525934860ba7a7df85928fa55baa0946f0cf1f
Gudmundur Ellert <gudmundur.ellert@gmail.com>
2019-10-22 04:16:15 +0000

    overview page is dynamic now

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour data from api
+  const tours = await Tour.find();
+  //build our pug template
+
+  //render that template with our data
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
